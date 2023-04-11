5502413e14e75cfa6db07e30c0e6dae16c2acb32
OleksandrDav <o.sec.dav@gmail.com>
2023-04-11 10:56:46 +0200

    Bootstrap added

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import Header from './components/Header/Header'
 import Main from './components/Main/Main'
 import Footer from './components/Footer/Footer'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
 
 function App() {
   return (
