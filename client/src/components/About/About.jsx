55bf11328b8b1128bab8a09770524b8a4a41637f
OleksandrDav <o.sec.dav@gmail.com>
2023-04-27 08:48:09 +0200

    deleted img from client

--- client/src/components/About/About.jsx
+++ client/src/components/About/About.jsx
@@ -7,6 +7,7 @@ function About() {
       <main className={s.main}>
          <div className={s.content}>
             <h1 className={s.contentTitle}>About</h1>
+            <p className={s.template}>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rem, nam sapiente impedit, laborum quisquam suscipit veritatis voluptatum enim quod error consectetur possimus delectus sequi excepturi alias rerum blanditiis pariatur quibusdam.</p>
          </div>
       </main>
    );
