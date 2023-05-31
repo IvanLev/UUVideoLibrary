6fa0920babade1c0fc885b2a855e463a8aa7e083
IvanLev <levitskiy.ivan@gmail.com>
2023-05-31 08:04:53 +0200

    Added validation to comment and video list ABLs

--- server/abl/comment/list-abl.js
+++ server/abl/comment/list-abl.js
@@ -1,18 +1,36 @@
 const path = require("path");
+const Ajv = require("ajv").default;
 const CommentDao = require("../../dao/comment-dao");
 let dao = new CommentDao(
     path.join(__dirname, "..", "..", "storage", "comments.json")
 );
 
+let schema = {
+    properties: {
+        video_id: { type: 'string' },
+    },
+    required: ["video_id"],
+};
+
 async function ListAbl(req, res) {
     try {
-        const { video_id } = req.query;
-        if(video_id) {
-            const commentList = await dao.listComments(video_id);
-            res.json(commentList);
+        const ajv = new Ajv();
+        const valid = ajv.validate(schema, req.query);
+        if (valid) {
+            const {video_id} = req.query;
+            if (video_id) {
+                const commentList = await dao.listComments(video_id);
+                res.json(commentList);
+            } else {
+                res.status(400).send({
+                    errorMessage: "invalid request",
+                });
+            }
         } else {
             res.status(400).send({
-                errorMessage: "invalid request",
+                errorMessage: "validation of input failed",
+                params: req.query,
+                reason: ajv.errors,
             });
         }
     } catch (e) {
--- server/abl/video/list-abl.js
+++ server/abl/video/list-abl.js
@@ -1,16 +1,35 @@
 const path = require("path");
+const Ajv = require("ajv").default;
 const VideoDao = require("../../dao/video-dao");
 let dao = new VideoDao(
     path.join(__dirname, "..", "..", "storage", "videos.json")
 );
 
+let schema = {
+    properties: {
+        genre: { type: 'string' },
+        search: { type: 'string' },
+        count: { type: 'string' }
+    },
+};
+
 async function ListAbl(req, res) {
     try {
-        let { genre, search, count } = req.query;
-        genre = genre || "";
-        search = search || "";
-        const videoList = await dao.filterVideos(genre, search, count);
-        res.json(videoList);
+        const ajv = new Ajv();
+        const valid = ajv.validate(schema, req.query);
+        if (valid) {
+            let {genre, search, count} = req.query;
+            genre = genre || "";
+            search = search || "";
+            const videoList = await dao.filterVideos(genre, search, count);
+            res.json(videoList);
+        } else {
+            res.status(400).send({
+                errorMessage: "validation of input failed",
+                params: req.query,
+                reason: ajv.errors,
+            });
+        }
     } catch (e) {
         console.log(e);
         res.status(500).send(e);
